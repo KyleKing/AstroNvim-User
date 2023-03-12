d304ba0867f2c4428f5f18216cbca8984bd49553
Kyle King <KyleKing@users.noreply.github.com>
2023-03-11 20:49:12 -0500

    feat: merge custom configuration

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,15 +2,45 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require("null-ls")
+    null_ls.setup({ debug = true })
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      -- > Formatting
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with({
+          prefer_local = "node_modules/.bin",
+      }),
+      null_ls.builtins.formatting.eslint.with({
+          prefer_local = "node_modules/.bin",
+      }),
+      null_ls.builtins.formatting.taplo,
+      null_ls.builtins.formatting.terraform_fmt,
+      null_ls.builtins.formatting.trim_newlines,
+      null_ls.builtins.formatting.trim_whitespace,
+
+      -- > Diagnostics
+      null_ls.builtins.diagnostics.eslint.with({
+        prefer_local = "node_modules/.bin",
+      }),
+      -- FIXME: How are project configuration files recognized?
+      null_ls.builtins.diagnostics.flake8.with({
+        only_local = true,
+      }),
+      -- null_ls.builtins.diagnostics.pylint.with({
+      --  only_local = true,
+      -- }),
+      -- null_ls.builtins.diagnostics.mypy.with({
+      --  only_local = true,
+      -- }),
+      null_ls.builtins.diagnostics.shellcheck,
+      null_ls.builtins.diagnostics.hadolint,
+
+      -- > Code Actions
+      null_ls.builtins.code_actions.gitsigns,
     }
     return config -- return final config table
   end,
