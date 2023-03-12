7c6b34ef26f90162484961e88ebb97171760d71f
Kyle King <KyleKing@users.noreply.github.com>
2023-03-11 20:49:12 -0500

    feat: merge custom configuration

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,3 +1,13 @@
+-- PLANNED: Consider these plugins
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/completion/copilot-lua-cmp
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/debugging/nvim-bqf
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/comment/mini-comment
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/diagnostics/trouble-nvim
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/media/vim-wakatime
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/color/ccc-nvim
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/motion
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/python
+--  https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/test/neotest (and https://github.com/nvim-neotest/neotest-python)
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
@@ -5,5 +15,22 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.nightfox" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.mini-splitjoin" },  -- Or treej
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,65 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  -- > Themes
+  -- "dracula/vim",  -- dracula
+  -- {  - FYI: Moved to astrocommunity version
+  --   "EdenEast/nightfox.nvim", -- nightfox, duskfox
+  --   -- PLANNED: These settings weren't being recognized?
+  --   opts = {
+  --     dim_inactive = true,
+  --     styles = {
+  --       comments = "italic",
+  --       keywords = "bold",
+  --       types = "italic,bold",
+  --     },
+  --   },
+  -- },
+  -- "folke/tokyonight.nvim",  -- tokyonight-storm
+  -- "joshdick/onedark.vim",  -- onedark
+  -- "rebelot/kanagawa.nvim",  -- kanagawa
+  -- "roflolilolmao/oceanic-next.nvim",  -- OceanicNext
+  -- {
+  --   "sainnhe/everforest", -- everforest
+  --   -- PLANNED: Need to figure out how configuration works. No lua examples
+  --   -- config = function()
+  --   --  set background=dark
+  --   --  let g:everforest_background = 'soft'
+  --   -- end,
+  --   --  background = "dark",
+  --   --  everforest_background = "hard",
+  --   --  everforest_enable_italic = 1,
+  --   --  everforest_dim_inactive_windows = 1,
+  --   --  everforest_sign_column_background = "grey",
+  --   --  everforest_ui_contrast = "high",
+  --   -- better_performance = 1,
+  -- },
+  -- "sickill/vim-monokai", -- monokai
+  -- "sonph/onehalf", -- onehalfdark
+
+  -- > Additional plugins
+  {
+    "sheerun/vim-polyglot",
+    event = "UIEnter",
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "codota/tabnine-nvim",
+    -- event = "BufRead",
+    config = function()
+      require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<Tab>",
+        debounce_ms = 300,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+      })
+    end,
+  },
 }
